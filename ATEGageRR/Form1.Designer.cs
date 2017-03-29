161bffe861a5c2157e42be1bfc02175414abe887
Edward <edward_song@yeah.net>
2017-03-29 20:56:48 +0800

    ATEGageRR

---
+++ ATEGageRR/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ATEGageRR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtExcelTemplate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMachine = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtEngineer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstFile = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtExcelTemplate
+            // 
+            this.txtExcelTemplate.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtExcelTemplate.Location = new System.Drawing.Point(751, 37);
+            this.txtExcelTemplate.Name = "txtExcelTemplate";
+            this.txtExcelTemplate.Size = new System.Drawing.Size(354, 25);
+            this.txtExcelTemplate.TabIndex = 0;
+            this.txtExcelTemplate.Visible = false;
+            this.txtExcelTemplate.DoubleClick += new System.EventHandler(this.txtExcelTemplate_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(631, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gage RR 模板";
+            this.label1.Visible = false;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGo.Location = new System.Drawing.Point(336, 174);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(260, 33);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Create Reports";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dtpDate.Location = new System.Drawing.Point(117, 21);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(169, 25);
+            this.dtpDate.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMachine);
+            this.groupBox1.Controls.Add(this.txtModel);
+            this.groupBox1.Controls.Add(this.txtEngineer);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtpDate);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 184);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "設置";
+            // 
+            // txtMachine
+            // 
+            this.txtMachine.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtMachine.Location = new System.Drawing.Point(117, 141);
+            this.txtMachine.Name = "txtMachine";
+            this.txtMachine.Size = new System.Drawing.Size(169, 25);
+            this.txtMachine.TabIndex = 11;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtModel.Location = new System.Drawing.Point(117, 104);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(169, 25);
+            this.txtModel.TabIndex = 10;
+            this.txtModel.TextChanged += new System.EventHandler(this.txtModel_TextChanged);
+            // 
+            // txtEngineer
+            // 
+            this.txtEngineer.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtEngineer.Location = new System.Drawing.Point(117, 61);
+            this.txtEngineer.Name = "txtEngineer";
+            this.txtEngineer.Size = new System.Drawing.Size(169, 25);
+            this.txtEngineer.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(24, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Machine";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(24, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(24, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Engineer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "GageRR開始時間";
+            // 
+            // lstFile
+            // 
+            this.lstFile.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lstFile.FormattingEnabled = true;
+            this.lstFile.HorizontalScrollbar = true;
+            this.lstFile.ItemHeight = 17;
+            this.lstFile.Location = new System.Drawing.Point(336, 38);
+            this.lstFile.Name = "lstFile";
+            this.lstFile.Size = new System.Drawing.Size(260, 123);
+            this.lstFile.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 249);
+            this.Controls.Add(this.lstFile);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtExcelTemplate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExcelTemplate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMachine;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtEngineer;
+        private System.Windows.Forms.ListBox lstFile;
+    }
+}
+
---
+++ ATEGageRR/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace ATEGageRR
+{
+    public partial class Form1 : Form
+    {
+
+        #region 參數定義
+        string gageDate = string.Empty;
+        string Engineer = string.Empty;
+        string Model = string.Empty;
+        string Machine = string.Empty;
+        string excelTemplate = string.Empty;
+       // Microsoft.Office.Interop.Excel.Application objExcel;
+        
+
+        #endregion
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "ATE Gage RR Report ,Ver:" + System.Windows.Forms.Application.ProductVersion;
+            if (!checkTime("410CD26EBAC024192F32F455445C4834"))
+            {
+                MessageBox.Show("軟件已過期,請重新申請授權使用!", "Error");
+                Environment.Exit(0);
+            }
+            this.txtExcelTemplate.SetWatermark("雙擊選擇Gage RR Excel 模板");
+            this.txtEngineer.SetWatermark("eg:Edward Song");
+            this.txtModel.SetWatermark("eg:Cedar-Hsw");
+            this.txtMachine.SetWatermark("eg:Cedar-Hsw-1");
+            gageDate = getDtpShortValue(dtpDate);           
+        }
+
+        #region OpenFile
+        /// <summary>
+        /// 打開文件,讓textbox顯示文件路徑
+        /// </summary>
+        /// <param name="textbox"></param>
+        public  void OpenFile(System.Windows.Forms.TextBox  textbox)
+        {
+            textbox.Text = string.Empty;
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "Excel 2003 file|*.xls|Excel file|*.xlsx|pdf file|*.pdf|boardview file|*.GR|all file|*.*";
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+                textbox.Text = openfiledialog.FileName;
+        }
+        #endregion
+
+        #region checkTime
+
+        /// <summary>
+        /// check time
+        /// </summary>
+        /// <param name="str">encrpt date</param>
+        /// <returns></returns>
+        public static bool checkTime(string str)
+        {
+            DateTime t = Convert.ToDateTime(DES.DesDecrypt(str, "Edward86"));
+
+            if (File.Exists("C:\\Windows\\System32\\" + System.Windows.Forms.Application.ProductName  + ".dll"))
+            {
+                return false;
+            }
+            FileInfo[] files = new DirectoryInfo("C:\\").GetFiles();
+            int index = 0;
+            while (index < files.Length)
+            {
+                if (DateTime.Compare(files[index].LastAccessTime, t) > 0)
+                {
+                    FileStream iniStram = File.Create("C:\\Windows\\System32\\" + System.Windows.Forms.Application.ProductName + ".dll");
+                    iniStram.Close();
+                    return false;
+                }
+                checked { ++index; }
+            }
+            return true;
+
+        }
+        #endregion
+
+        #region getDtpValue
+        /// <summary>
+        /// 從DateTimePicker獲取獲取短日期信息
+        /// </summary>
+        /// <param name="dtp"></param>
+        /// <returns></returns>
+        public static string getDtpShortValue(DateTimePicker dtp)
+        {
+            string value = string.Empty;
+            try
+            {
+                value = string.Format("{0:D4}", dtp.Value.Year) + "/" + string.Format("{0:D2}", dtp.Value.Month) + "/" + string.Format("{0:D2}", dtp.Value.Day);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return value;
+        }
+        #endregion
+
+        #region KillExcel
+        private void KillExcel()
+        {
+            System.Diagnostics.Process[] excelProcess = System.Diagnostics.Process.GetProcessesByName("EXCEL");
+            foreach (System.Diagnostics.Process p in excelProcess)
+                p.Kill();
+        }
+        #endregion
+
+
+        private void txtExcelTemplate_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFile(txtExcelTemplate);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            
+           // checkInput();
+            if (!checkInput())
+                return;
+            this.Enabled = false;
+            lstFile.Items.Clear();
+           
+            //download excelTemplate
+            string tempFilePath = System.Windows.Forms.Application.StartupPath + @"\GageRRTemplate.xls";
+            if (downLoadFile(tempFilePath))
+            {
+                excelTemplate = tempFilePath;
+            }
+            else
+            {
+                this.Enabled = true;
+                return;
+            }
+
+            //create folder
+            string folderPath = System.Windows.Forms.Application.StartupPath + @"\" + this.txtMachine.Text;
+            if (!Directory.Exists(folderPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Create " + folderPath + " Fail,Message:" + ex.Message);
+                    this.Enabled = true;
+                    return;
+                  
+                }
+            }
+
+           
+
+            do
+            {
+                gageDate = getDtpShortValue(dtpDate);
+                string fileName = folderPath + @"\Gage_R&R_Report_" + Machine + "_" + gageDate.Replace("/", string.Empty) + ".xls";
+                createGageRR (gageDate ,Engineer,Model,Machine,fileName );
+                dtpDate.Value = dtpDate.Value.AddMonths(3);
+                
+            } while (dtpDate.Value < DateTime.Now );
+            this.Enabled = true;
+        }
+
+
+
+
+        private bool createGageRR(string gagedate, string enginner, string model, string machine,string fileName)
+        {
+            Microsoft.Office.Interop.Excel.Application objExcel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook objWorkbook = objExcel.Workbooks.Add(excelTemplate);
+
+
+            objExcel.Cells[3, 7] = gagedate;
+            objExcel.Cells[4, 7] = enginner;
+            objExcel.Cells[5, 7] = model;
+            objExcel.Cells[6, 7] = machine;
+
+            objExcel.DisplayAlerts = false;
+            objExcel.AlertBeforeOverwriting = false;
+            objWorkbook.SaveAs(fileName);
+            objWorkbook.Close();
+            objExcel.SaveWorkspace(fileName);
+            objExcel.Quit();
+            objExcel = null;
+            //objWorkbook.SaveAs(fileName);
+            KillExcel();
+
+            // fileName = fileFullPath.Substring(fileFullPath.LastIndexOf("\\") + 1, fileFullPath.Length - fileFullPath.LastIndexOf("\\") - 1);
+           // lstFile .Items.Add (fileName.Substring (fileName.LastIndexOf ("\\") +1,fileName.Length -fileName.LastIndexOf ("\\") -1));
+
+
+            return true;
+        }
+
+
+        private bool  checkInput()
+        {
+            //check input
+            //if (string.IsNullOrEmpty(txtExcelTemplate.Text.Trim()))
+            //{
+            //    MessageBox.Show("請選擇Excel模板");
+            //    return false;
+            //}
+            //else
+            //{
+            //    excelTemplate = txtExcelTemplate.Text.Trim();
+            //}
+            //check engineer
+            if (string.IsNullOrEmpty(txtEngineer.Text.Trim()))
+            {
+                MessageBox.Show("Engineer 不能為空");
+                return false;
+            }
+            else
+            {
+                Engineer = this.txtEngineer.Text.Trim();
+            }
+            //check model
+            if (string.IsNullOrEmpty(txtModel.Text.Trim()))
+            {
+                MessageBox.Show("Model 不能為空");
+                return false;
+            }
+            else
+            {
+                Model = this.txtModel.Text.Trim();
+            }
+            //check machine
+            if (string.IsNullOrEmpty(txtMachine.Text.Trim()))
+            {
+                MessageBox.Show("Machine 不能為空");
+                return false;
+            }
+            else
+            {
+                Machine = txtMachine.Text.Trim();
+            }
+            return true;
+        }
+
+
+
+        #region getDtpValue
+        /// <summary>
+        /// 從DateTimePicker獲取獲取短日期信息
+        /// </summary>
+        /// <param name="dtp"></param>
+        /// <returns></returns>
+        public static string getDtpShortStringValue(DateTimePicker dtp)
+        {
+            string value = string.Empty;
+            try
+            {
+                value = string.Format("{0:D4}", dtp.Value.Year) +  string.Format("{0:D2}", dtp.Value.Month) +  string.Format("{0:D2}", dtp.Value.Day);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return value;
+        }
+        #endregion
+
+        private void txtModel_TextChanged(object sender, EventArgs e)
+        {
+            this.txtMachine.Text = this.txtModel.Text;
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+
+  
+                //MessageBox.Show("OK"); 
+        }
+
+
+        private bool downLoadFile(string filePath)
+        {
+
+            if (!File.Exists(filePath))
+            {
+                byte[] template = Properties.Resources.GageRRTemplate;
+                FileStream stream = new FileStream(filePath, FileMode.Create);
+                try
+                {
+                    stream.Write(template, 0, template.Length);
+                    stream.Close();
+                    stream.Dispose();
+                    File.SetAttributes(filePath, FileAttributes.Hidden);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Create GageRRTempalte Fail,Message:" + ex.Message);
+                    return false;
+                }     
+          
+            }
+            return true;
+        }
+    }
+}
